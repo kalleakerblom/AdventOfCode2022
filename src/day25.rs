b25fe42ae4ee212afa2bb93764f23e3dc98f45ba
kalleakerblom <kalleakerblom@gmail.com>
2023-01-03 10:07:25 +0100

    Refactor day 25

--- src/day25.rs
+++ src/day25.rs
@@ -1,53 +1,48 @@
-fn snafu_to_decimal(snafu: &[char]) -> i64 {
+fn snafu_to_decimal(snafu: &str) -> i64 {
     snafu
-        .iter()
+        .bytes()
         .rev()
         .enumerate()
         .map(|(i, c)| match c {
-            '0' => 0,
-            '1' => 5i64.pow(i as u32),
-            '2' => 2 * 5i64.pow(i as u32),
-            '-' => -(5i64.pow(i as u32)),
-            '=' => -2 * 5i64.pow(i as u32),
+            b'0' => 0,
+            b'1' => 1,
+            b'2' => 2,
+            b'-' => -1,
+            b'=' => -2,
             _ => panic!(),
-        })
+        } * 5i64.pow(i as u32))
         .sum()
 }
-const SNAFU: [(i64, char); 5] = [(-2, '='), (-1, '-'), (0, '0'), (1, '1'), (2, '2')];
-fn decimal_to_snafu(i: i64) -> Vec<char> {
+const SNAFU: [(i8, u8); 5] = [(-2, b'='), (-1, b'-'), (0, b'0'), (1, b'1'), (2, b'2')];
+fn decimal_to_snafu(i: u64) -> String {
     let mut current = 0;
-    let mut res = vec![];
-
+    let mut symbols = 0;
     for exp in 0u32.. {
         if current > i {
             break;
         }
-        current += 2 * 5i64.pow(exp);
-        res.push('2');
+        current += 2 * 5u64.pow(exp);
+        symbols += 1;
     }
+    let mut res = vec![b'2'; symbols];
     let len = res.len() as u32;
     for (exp, c) in res.iter_mut().enumerate() {
-        let term = 5i64.pow(len - 1 - exp as u32);
-        for (factor, symbol) in &SNAFU[..4] {
-            if (current - (2 - factor) * term) >= i {
-                current -= (2 - factor) * term;
+        let five_pow = 5u64.pow(len - 1 - exp as u32);
+        for (val, symbol) in &SNAFU[..4] {
+            let sub = (2 - val) as u64 * five_pow;
+            if current >= sub + i {
+                current -= sub;
                 *c = *symbol;
                 break;
             }
         }
     }
-    res
+    unsafe { String::from_utf8_unchecked(res) }
 }
 
 pub fn part_1(input: &str) -> String {
-    let sum: i64 = input
-        .lines()
-        .map(|l| {
-            let snafu: Vec<_> = l.chars().collect();
-            snafu_to_decimal(&snafu)
-        })
-        .sum();
-    decimal_to_snafu(sum).iter().collect()
+    let sum: i64 = input.lines().map(snafu_to_decimal).sum();
+    decimal_to_snafu(sum as u64)
 }
 
 #[cfg(test)]
@@ -56,11 +51,13 @@ mod tests {
     use std::fs;
     #[test]
     fn sna_to_dec() {
-        assert_eq!(snafu_to_decimal(&['1', '=', '-', '0', '-', '2']), 1747);
+        assert_eq!(snafu_to_decimal("1=-0-2"), 1747);
     }
     #[test]
     fn dec_to_sna() {
-        assert_eq!(decimal_to_snafu(1747), vec!['1', '=', '-', '0', '-', '2']);
+        for _ in 0..1_000_000 {
+            assert_eq!(decimal_to_snafu(1747), "1=-0-2");
+        }
     }
 
     #[test]
