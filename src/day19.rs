75aa918bde09a2665579bc356b75c5b0b9904a38
kalleakerblom <kalleakerblom@gmail.com>
2023-01-08 12:01:52 +0100

    refactor day19 to use strong types

--- src/day19.rs
+++ src/day19.rs
@@ -1,30 +1,39 @@
+use derive_more::{Add, AddAssign, From, SubAssign};
 use scan_fmt::scan_fmt;
 use std::{cmp, collections::HashMap};
-struct ObsBotCost {
-    ore: u16,
-    clay: u16,
-}
-struct GeoBotCost {
-    ore: u16,
-    obs: u16,
-}
+#[derive(
+    Clone, Copy, Default, Hash, PartialEq, Eq, From, AddAssign, Add, SubAssign, PartialOrd, Ord,
+)]
+struct Ore(u16);
+#[derive(
+    Clone, Copy, Default, Hash, PartialEq, Eq, From, AddAssign, Add, SubAssign, PartialOrd, Ord,
+)]
+struct Clay(u16);
+#[derive(
+    Clone, Copy, Default, Hash, PartialEq, Eq, From, AddAssign, Add, SubAssign, PartialOrd, Ord,
+)]
+struct Obs(u16);
+#[derive(
+    Clone, Copy, Default, Hash, PartialEq, Eq, From, AddAssign, Add, SubAssign, PartialOrd, Ord,
+)]
+struct Geo(u16);
 
 struct Bp {
-    ore_bot_cost: u16,  //Ore
-    clay_bot_cost: u16, //Ore
-    obs_bot_cost: ObsBotCost,
-    geo_bot_cost: GeoBotCost,
+    ore_bot_cost: Ore,
+    clay_bot_cost: Ore,
+    obs_bot_cost: (Ore, Clay),
+    geo_bot_cost: (Ore, Obs),
 }
-#[derive(Default, Clone, Copy, Debug, Hash, PartialEq, Eq)]
+#[derive(Default, Clone, Copy, Hash, PartialEq, Eq)]
 struct State {
-    ore_bot: u16,
-    clay_bot: u16,
-    obs_bot: u16,
-    geo_bot: u16,
-    clay: u16,
-    ore: u16,
-    obs: u16,
-    geo: u16,
+    ore_bot: Ore,
+    clay_bot: Clay,
+    obs_bot: Obs,
+    geo_bot: Geo,
+    ore: Ore,
+    clay: Clay,
+    obs: Obs,
+    geo: Geo,
 }
 
 impl Bp {
@@ -32,16 +41,10 @@ impl Bp {
         let (_id, ob_cost, cb_cost, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs) =
             scan_fmt!(s, "Blueprint {}: Each ore robot costs {} ore. Each clay robot costs {} ore. Each obsidian robot costs {} ore and {} clay. Each geode robot costs {} ore and {} obsidian.", u16,u16, u16, u16, u16, u16, u16).unwrap();
         Self {
-            ore_bot_cost: ob_cost,
-            clay_bot_cost: cb_cost,
-            obs_bot_cost: ObsBotCost {
-                ore: obs_cost_ore,
-                clay: obs_cost_clay,
-            },
-            geo_bot_cost: GeoBotCost {
-                ore: geo_cost_ore,
-                obs: geo_cost_obs,
-            },
+            ore_bot_cost: Ore(ob_cost),
+            clay_bot_cost: Ore(cb_cost),
+            obs_bot_cost: (Ore(obs_cost_ore), Clay(obs_cost_clay)),
+            geo_bot_cost: (Ore(geo_cost_ore), Obs(geo_cost_obs)),
         }
     }
 }
@@ -51,8 +54,8 @@ fn play(
     bp: &Bp,
     mut state: State,
     time: u16,
-    best: &mut u16,
-    memo: &mut HashMap<(u16, State), u16>,
+    best: &mut Geo,
+    memo: &mut HashMap<(u16, State), Geo>,
 ) {
     if time == 0 {
         *best = cmp::max(*best, state.geo);
@@ -68,13 +71,13 @@ fn play(
     }
     // try all spends
     //GeoBot
-    if state.ore >= bp.geo_bot_cost.ore + state.ore_bot
-        && state.obs >= bp.geo_bot_cost.obs + state.obs_bot
+    if state.ore >= bp.geo_bot_cost.0 + state.ore_bot
+        && state.obs >= bp.geo_bot_cost.1 + state.obs_bot
     {
         let mut new_state = state;
-        new_state.ore -= bp.geo_bot_cost.ore;
-        new_state.obs -= bp.geo_bot_cost.obs;
-        new_state.geo_bot += 1;
+        new_state.ore -= bp.geo_bot_cost.0;
+        new_state.obs -= bp.geo_bot_cost.1;
+        new_state.geo_bot += 1.into();
         play(bp, new_state, time - 1, best, memo);
         // cache best
         if time < CACHE_TIME {
@@ -84,31 +87,31 @@ fn play(
     }
 
     // ObsBot
-    if state.ore >= bp.obs_bot_cost.ore + state.ore_bot
-        && state.clay >= bp.obs_bot_cost.clay + state.clay_bot
-        && state.obs_bot < bp.geo_bot_cost.obs
+    if state.ore >= bp.obs_bot_cost.0 + state.ore_bot
+        && state.clay >= bp.obs_bot_cost.1 + state.clay_bot
+        && state.obs_bot < bp.geo_bot_cost.1
     {
         let mut new_state = state;
-        new_state.ore -= bp.obs_bot_cost.ore;
-        new_state.clay -= bp.obs_bot_cost.clay;
-        new_state.obs_bot += 1;
+        new_state.ore -= bp.obs_bot_cost.0;
+        new_state.clay -= bp.obs_bot_cost.1;
+        new_state.obs_bot += 1.into();
         play(bp, new_state, time - 1, best, memo);
     }
     //ClayBot
-    if state.ore >= bp.clay_bot_cost + state.ore_bot && state.clay_bot < bp.obs_bot_cost.clay {
+    if state.ore >= bp.clay_bot_cost + state.ore_bot && state.clay_bot < bp.obs_bot_cost.1 {
         let mut new_state = state;
         new_state.ore -= bp.clay_bot_cost;
-        new_state.clay_bot += 1;
+        new_state.clay_bot += 1.into();
         play(bp, new_state, time - 1, best, memo);
     }
     // OreBot
     let max_ore_cost = bp
         .clay_bot_cost
-        .max(bp.obs_bot_cost.ore.max(bp.geo_bot_cost.ore));
+        .max(bp.obs_bot_cost.0.max(bp.geo_bot_cost.0));
     if state.ore >= bp.ore_bot_cost + state.ore_bot && state.ore_bot < max_ore_cost {
         let mut new_state = state;
         new_state.ore -= bp.ore_bot_cost;
-        new_state.ore_bot += 1;
+        new_state.ore_bot += 1.into();
         play(bp, new_state, time - 1, best, memo);
     }
     // No bot built
@@ -125,19 +128,19 @@ pub fn part_1(input: &str) -> usize {
         .iter()
         .enumerate()
         .map(|(id, bp)| {
-            let mut best = 0;
+            let mut best = 0.into();
             let mut memo = HashMap::new();
             play(
                 bp,
                 State {
-                    ore_bot: 1,
+                    ore_bot: 1.into(),
                     ..Default::default()
                 },
                 24,
                 &mut best,
                 &mut memo,
             );
-            (id + 1) * best as usize
+            (id + 1) * best.0 as usize
         })
         .sum()
 }
@@ -147,19 +150,19 @@ pub fn part_2(input: &str) -> usize {
         .iter()
         .take(3)
         .map(|bp| {
-            let mut best = 0;
+            let mut best = 0.into();
             let mut memo = HashMap::new();
             play(
                 bp,
                 State {
-                    ore_bot: 1,
+                    ore_bot: 1.into(),
                     ..Default::default()
                 },
                 32,
                 &mut best,
                 &mut memo,
             );
-            best as usize
+            best.0 as usize
         })
         .product()
 }
